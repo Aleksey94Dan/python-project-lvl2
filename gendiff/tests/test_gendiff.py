70c27b665e40db6c4414732514f8a7c73155a87d
Aleksey94Dan <danilchenko.aleksey94@gmail.com>
2019-12-26 16:21:47 +0700

    Temporary. It needs to change

--- gendiff/tests/test_gendiff.py
+++ gendiff/tests/test_gendiff.py
@@ -5,7 +5,7 @@ from gendiff.generate_diff import generate_diff
 def test_gendiff():
     with open('/home/all_done/Hexlet/projects/python-project-lvl2/gendiff/tests/fixtures/diff.txt') as diff_file:
         a = diff_file.read()
-    b = generate_diff('gendiff/tests/fixtures/after.json', 
+    b = generate_diff('gendiff/tests/fixtures/after.json',
     'gendiff/tests/fixtures/before.json')
     assert a == b
 
