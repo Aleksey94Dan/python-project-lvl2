735ae5a4825888fa575216f9b1df8c17db100f7b
Aleksey Danilchenko <danilchenko.aleksey94@gmail.com>
2019-12-26 22:30:09 +0700

    Test script

--- gendiff/tests/test_gendiff.py
+++ gendiff/tests/test_gendiff.py
@@ -1,11 +1,18 @@
-import pytest
-from gendiff.generate_diff import generate_diff
+# -*- coding:utf-8 -*-
+
+"""Test package."""
+
+from gendiff.generate_diff import generate_diff, get_files
 
 
 def test_gendiff():
-    with open('/home/all_done/Hexlet/projects/python-project-lvl2/gendiff/tests/fixtures/diff.txt') as diff_file:
-        a = diff_file.read()
-    b = generate_diff('gendiff/tests/fixtures/after.json',
-    'gendiff/tests/fixtures/before.json')
-    assert a == b
-
+    """Function testing generate_diff."""
+    path_to_expected_file = get_files('gendiff/tests/fixtures/diff.txt')
+    path_to_first_file = 'gendiff/tests/fixtures/after.json'
+    path_to_second_file = 'gendiff/tests/fixtures/before.json'
+    with open(path_to_expected_file) as diff_file:
+        expected_data = diff_file.read()
+    assert expected_data == generate_diff(
+        path_to_first_file,
+        path_to_second_file,
+    )
