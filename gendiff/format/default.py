4c5266c7c8cc2c759efd3e6be338b00e0df196b9
Aleksey94Dan <50262563+Aleksey94Dan@users.noreply.github.com>
2020-09-29 11:16:17 +0300

    Update default.py

--- gendiff/format/default.py
+++ gendiff/format/default.py
@@ -5,22 +5,24 @@
 SIGN = '  '
 
 
+def _inner(node, indent=0, string=None):
+    string = string if string else ''
+    for vertex, child in node.items():
+        if isinstance(child, bool):
+            child = str(child).lower()
+        if ' ' not in str(vertex):
+            vertex = SIGN + str(vertex)
+        if isinstance(child, dict):
+            string = string + '{0}{1} {{\n'.format(indent * SIGN, vertex)
+            string = string + _inner(child, indent + 2)
+            string = string + '{0}}}\n'.format((indent + 2) * SIGN)
+        else:
+            string = string + '{0}{1}: {2}\n'.format(
+                SIGN * indent, str(vertex), str(child),
+            )
+    return string
+
+
 def format_(source):
     """Print default."""
-    def inner(node, indent=0, string=None):  # noqa: WPS430
-        string = string if string else ''
-        for vertex, child in node.items():
-            if isinstance(child, bool):
-                child = str(child).lower()
-            if ' ' not in str(vertex):
-                vertex = SIGN + str(vertex)
-            if isinstance(child, dict):
-                string = string + '{0}{1} {{\n'.format(indent * SIGN, vertex)
-                string = string + inner(child, indent + 2)
-                string = string + '{0}}}\n'.format((indent + 2) * SIGN)
-            else:
-                string = string + '{0}{1}: {2}\n'.format(
-                    SIGN * indent, str(vertex), str(child),
-                )
-        return string
-    return '{{\n{0}}}\n'.format(inner(source))
+    return '{{\n{0}}}\n'.format(_inner(source))
