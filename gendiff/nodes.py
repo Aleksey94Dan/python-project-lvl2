8bfb8ddcb7dee4f39fd8ff807a9b038556b3454b
Aleksey94Dan <danilchenko.aleksey94@gmail.com>
2020-10-23 21:55:16 +0700

    update nodes

--- gendiff/nodes.py
+++ gendiff/nodes.py
@@ -1,85 +1,48 @@
 # -*- coding:utf-8 -*-
 
 
-"""The module describes the rules for building AST."""
-from operator import itemgetter
+"""The module describes the rules for building Tree."""
 
 ADDED = 'added'
 DELETED = 'deleted'
-UNCHANGEABLE = 'unchangeable'
-CHANGEABLE = 'changeable'
+UNCHANGED = 'unchanged'
+CHANGED = 'changed'
 STATUS = 'status'
-KEY = 'key'
 VALUE = 'value'
-NEW_VALUE = 'new_value'
 OLD_VALUE = 'old_value'
-CHILDREN = 'children'
-NAME = 'name'
 
 
-def make_node(name, status=None, children=None, current_value=None):
-    """Return node."""
-    return {
-        NAME: name,
-        STATUS: status,
-        CHILDREN: children,
-        VALUE: current_value,
-    }
-
-
-def make_tree(before_file, after_file):  # noqa: WPS210
+def make_tree(old_file, new_file):  # noqa: WPS210
     """Return an abstract syntax tree."""
-    acc = []
-    common_keys = before_file.keys() & after_file.keys()
-    added_keys = after_file.keys() - before_file.keys()
-    deleted_keys = before_file.keys() - after_file.keys()
+    acc = {}
 
-    for common_key in common_keys:
-        before_value = before_file.get(common_key)
-        after_value = after_file.get(common_key)
-        if isinstance(after_value, dict) and isinstance(before_value, dict):
-            acc.append(
-                make_node(
-                    name=common_key,
-                    children=make_tree(before_value, after_value),
-                ),
-            )
-        elif after_value == before_value:
-            acc.append(
-                make_node(
-                    name=common_key,
-                    status=UNCHANGEABLE,
-                    current_value=before_value,
-                ),
-            )
+    for common_key in old_file.keys() & new_file.keys():
+        old_value = old_file.get(common_key)
+        new_value = new_file.get(common_key)
+
+        if all((isinstance(old_value, dict), isinstance(new_value, dict))):
+            acc[common_key] = make_tree(old_value, new_value)
+        elif new_value == old_value:
+            acc[common_key] = {
+                STATUS: UNCHANGED,
+                VALUE: old_value,
+            }
         else:
-            acc.append(
-                make_node(
-                    name=common_key,
-                    status=CHANGEABLE,
-                    current_value={
-                        OLD_VALUE: before_value,
-                        NEW_VALUE: after_value,
-                    },
-                ),
-            )
+            acc[common_key] = {
+                STATUS: CHANGED,
+                VALUE: new_value,
+                OLD_VALUE: old_value,
+            }
 
-    for added_key in added_keys:
-        acc.append(
-            make_node(
-                name=added_key,
-                status=ADDED,
-                current_value=after_file.get(added_key),
-            ),
-        )
+    for added_key in new_file.keys() - old_file.keys():
+        acc[added_key] = {
+            STATUS: ADDED,
+            VALUE: new_file.get(added_key),
+        }
 
-    for deleted_key in deleted_keys:
-        acc.append(
-            make_node(
-                name=deleted_key,
-                status=DELETED,
-                current_value=before_file.get(deleted_key),
-            ),
-        )
-    acc.sort(key=itemgetter(NAME), reverse=False)
+    for deleted_key in old_file.keys() - new_file.keys():
+        acc[deleted_key] = {
+            STATUS: DELETED,
+            VALUE: old_file.get(deleted_key),
+        }
     return acc
