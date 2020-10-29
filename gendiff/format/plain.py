3b6af9a2da4a75ca6bda4a13c3081e45776e6165
Aleksey94Dan <danilchenko.aleksey94@gmail.com>
2020-10-29 22:55:10 +0700

    add function _flatten and update mapping

--- gendiff/format/plain.py
+++ gendiff/format/plain.py
@@ -6,7 +6,6 @@ from gendiff import nodes
 
 COMPLEX_VALUE = '[complex value]'
 
-
 _get_template = {
     nodes.ADDED: "Property '{0}' was added with value: {1}",
     nodes.DELETED: "Property '{0}' was removed",
@@ -15,36 +14,50 @@ _get_template = {
 }.get
 
 
-def mapping(node, path=None, acc=None):  # noqa: WPS210
-    """Return maps for format plain."""
-    path = path if path else []
+def _flatten(subtree, acc=None):
     acc = acc if acc else []
-    for k, v in sorted(node.items(), reverse=True):  # noqa: WPS111
-        new_path = path.copy()
-        new_path.append(k)
-        status = v.get(nodes.STATUS)
-        if status:
-            new_path.append((
-                status,
-                _helper(v.get(nodes.OLD_VALUE)),
-                _helper(v.get(nodes.VALUE)),
-            ),
-            )
-        elif isinstance(v, dict):
-            mapping(v, new_path, acc)
-        acc.append(new_path)
+    for node in subtree:
+        if isinstance(node, list):
+            _flatten(node, acc)
+        else:
+            acc.append(node)
     return acc
+
+
+def mapping(node, key=None, ancestry=None):
+    """Return maps for format plain."""
+    ancestry = ancestry if ancestry else []
+    status = node.get(nodes.STATUS)
+    new_ancestry = ancestry.copy()
+    new_ancestry.append(key)
+    if status:
+        return (
+            '.'.join(filter(None, new_ancestry)),
+            status,
+            _helper(node.get(nodes.OLD_VALUE)),
+            _helper(node.get(nodes.VALUE)),
+        )
+    paths = list(
+        map(
+            lambda x: mapping(  # noqa: WPS111
+                x[1],
+                x[0],
+                new_ancestry,
+            ),
+            node.items(),
+        ),
+    )
+    return _flatten(paths)
 
 
 def format(source):  # noqa: A001, WPS210
     """Print plain."""
-    source = filter(lambda x: isinstance(x[-1], tuple), source)  # noqa: WPS111
     string = []
     for package in source:
-        *origin, (status, old_value, new_value) = package  # noqa: WPS414
+        origin, status, old_value, new_value = package
         string.append(
             _get_template(status).format(
-                '.'.join(origin),
+                origin,
                 new_value,
                 old_value,
             ),
@@ -59,4 +72,3 @@ def _helper(arg1):
     elif isinstance(arg1, dict):
         return COMPLEX_VALUE
     return "'{0}'".format(arg1)
-
